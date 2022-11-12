db5b5270f41b3fdd6b52a527be2d9e7a949f7187
Breno <101889347+SenhorDoritos@users.noreply.github.com>
2022-11-11 22:09:56 -0300

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    question: "Que música Manoel Gomes criou?",
+    answers: [
+      { option: "Summertime Sadness", correct: false },
+      { option: "Trem-Bala", correct: false },
+      { option: "Caneta Azul", correct: true },
+    ],
+  },
+  {
+    question: "4 + 4?",
+    answers: [
+      { option: "5", correct: true },
+      { option: "8", correct: false },
+      { option: "0", correct: false },
+    ],
+  },
+  {
+    question: "Quem é Daniel Orivaldo da Silva?",
+    answers: [
+      { option: "Kid Bengala", correct: false },
+      { option: "Galo Cego", correct: true },
+      { option: "Irineu", correct: false },
+    ],
+  },
+  {
+    question: "Bebida favorita do Jailson Mendes?",
+    answers: [
+      { option: "Cerveja", correct: false },
+      { option: "Tequila", correct: false },
+      { option: "Suco de laranja", correct: true },
+    ],
+  },
+  {
+    question: "Qual é a maior baleia do mundo?",
+    answers: [
+      { option: "Baleia-azul", correct: true },
+      { option: "Baleia-fin", correct: false },
+      { option: "Thais Carla", correct: true },
+    ],
+  },
+  {
+    question: "Eu vou lhe achar...",
+    answers: [
+      { option: "Pukémon", correct: true },
+      { option: "Pokémon", correct: false },
+      { option: "Pikomon", correct: true },
+    ],
+  },
+];
